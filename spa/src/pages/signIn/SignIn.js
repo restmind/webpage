d012536491c6b36a18a6e77c20bf9eaa87f0513b
Shuplat Orest <shuplatorst@gmail.com>
2021-01-17 17:39:35 +0200

    Adding post method to login page

--- spa/src/pages/signIn/SignIn.js
+++ spa/src/pages/signIn/SignIn.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Buttons/Button.styled";
-import { ContentImg, SignInText, Content, SignInInputs, DontHaveAcc, GoogleButton, BackgroundWrapper, ContentWrapper, LoginBtnLink } from "./SignIn.styled";
+import { ContentImg, SignInText, Content, SignInInputs, DontHaveAcc, GoogleButton, BackgroundWrapper, ContentWrapper, LoginBtnLink, StyledInput } from "./SignIn.styled";
 
-export const SignIn = () => (
+function SignIn() {
+    const [user, setUser] = useState(
+        {
+            username:'',
+            password:''
+        }
+    );
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setUser(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+    const handleClick = () => {
+        var apiBaseUrl = "http://localhost:4000/api/";
+        var payload={
+        "email": user.username,
+        "password": user.password
+        }
+        console.log(user);
+        axios.post(apiBaseUrl+'login', payload)
+        .then(function (response) {
+        console.log(response);
+        if(response.data.code == 200){
+        console.log("Login successfull");
+        }
+        else if(response.data.code == 204){
+        console.log("Username password do not match");
+        alert("username password do not match")
+        }
+        else{
+        console.log("Username does not exists");
+        alert("Username does not exist");
+        }
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+    };
+
+    return (
     <React.Fragment>
         <BackgroundWrapper>
             <ContentWrapper>
@@ -15,9 +57,9 @@ export const SignIn = () => (
                         <GoogleButton><img src="\images\icon-google.svg" alt="google icon"></img> Sign In with Google</GoogleButton>
                         <hr data-content="OR"></hr>
                         <label>Email adress</label>
-                        <input type="email"></input>
+                        <StyledInput value={user.username} type="email" onChange={handleChange} name="username"></StyledInput>
                         <label>Password</label>
-                        <input type="password"></input>
+                        <StyledInput value={user.password} type="password" onChange={handleChange} name="password"></StyledInput>
                         <DontHaveAcc>
                             <h5>Don't have an account?</h5>
                             <Link to="/sign-up">
@@ -25,7 +67,7 @@ export const SignIn = () => (
                             </Link>
                         </DontHaveAcc>
                         <LoginBtnLink to="/">
-                            <Button primary>Log In</Button>
+                            <Button primary htmlType="submit" onClick={handleClick}>Log In</Button>
                         </LoginBtnLink>
                     </SignInInputs>
                     <ContentImg>
@@ -36,5 +78,6 @@ export const SignIn = () => (
         </BackgroundWrapper>
     </React.Fragment>
 );
+}
 
 export default SignIn;
